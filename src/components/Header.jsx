01852110afdbd5f8a6224ec67e7491e11db0aeed
safinxr <safinxr@gmail.com>
2023-09-14 21:12:26 +0600

    header part done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return(
         <header className="max-w-7xl mx-auto">
-            
+            <h1 className="text-4xl text-center font-bold my-12">Course Registration</h1>
         </header>
     )
 };
